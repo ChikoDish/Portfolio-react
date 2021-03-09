15d5dab6d52b832a771ed60799bab2b9c00b99cb
Ankit Kumar Gupta <ankitkumargupta00@gmail.com>
2021-03-09 20:31:49 +0530

    Styled a little

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import { Canvas } from "react-three-fiber";
 import Box from "./components/Box";
 function App() {
   return (
-    <Canvas style={{ height: 500, width: 1200 }}>
-      <ambientLight intensity={0.5} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} />
-      <Box position={[2, -4, 0]} />
-    </Canvas>
+    <>
+      <Canvas style={{ height: 500, width: 1200 }}>
+        <ambientLight intensity={0.5} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+        <pointLight position={[-10, -10, -10]} />
+        <Box position={[2, -4, 0]} />
+      </Canvas>
+      <div className="middle-container">
+        <h1>I'm Ankit Kumar Gupta</h1>
+        <artical>
+          Software Engineer, I'm passionate about learing new things and getting
+          upgraded.
+        </artical>
+      </div>
+    </>
   );
 }
 
